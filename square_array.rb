0744ae2e61b1709acf86950ea8a858ca56dbd704
Rama Milaneh <ramamilaneh@gmail.com>
2016-08-06 21:07:18 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  new_array=[]
+  array.each do |element|
+    new_array.push(element**2)
+  end
+  new_array
 end
+#def square_array(array)
+  #array.collect do |element|
+    #element**2
+  #end
+#end     
