4e04809fc2af28fa709371476f512245aa73f189
Starsky Eustache <star.eu88@gmail.com>
2016-08-15 02:52:02 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+
 def square_array(array)
-  # your code here
+    temp = Array.new(3)
+    index = 0
+    array.each do |num|
+        index = index + 1
+        temp[index-1] = num * num
+        #print num * num
+        puts temp[index-1]
+    end
+    array = Array.new(temp)
+    #puts array
 end
