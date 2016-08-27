ad699bbd587c6d8670764357ba836663f19a7d0e
Jad Abdallah <justcallmethis@gmail.com>
2016-08-27 17:09:14 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
+# def square_array(array)
+#   # it doesn't affect them
+#      array.each do |num|
+#     num * num
+#
+#   end
+# end
+
+
+
 def square_array(array)
-  # your code here
+    array1 = []
+    array.each do |num|
+    array1 << (num * num)
+  end
+  array1
 end
