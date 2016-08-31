4e7746d2c5c4fcc2ce21da64728becd4334c990c
Gabriela Pinto <gajspp@gmail.com>
2016-08-31 01:28:15 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+    array_length = array.length - 1
+    i = 0
+    while i <= array_length && >= 0
+      numbers = array.each
+      result = numbers**numbers
+      new_array.push(result)
+      i = i + 1
+      return new_array
+    end
 end
