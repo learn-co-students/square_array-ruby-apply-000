769950c4516014c99ff0eebbc203caf18c8ca6e1
Justin Peck <jdpeck90@gmail.com>
2016-09-01 14:50:51 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
 def square_array(array)
-  # your code here
-end
+i = 0
+
+array.each do |x|
+  array[i] = x*x 
+  i += 1
+    end
+   end
+
+=begin This also works!
+   def square_array(array)
+   i = 0
+
+   array.each do |x|
+     array[i] = x**2
+     i += 1
+       end
+      end
+=end
