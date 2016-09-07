20a8d0d633df0637e82ef6f1ef5b05a4f35487a6
Sally Whang <wsaehee21@outlook.com>
2016-09-07 13:05:49 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |squared_num|
+    new_array << "#{squared_num}".to_i ** 2
+  end
+
+  return new_array
 end
+
+# def square_array(array)
+#   array.collect do |squared_num|
+#     "#{squared_num}".to_i ** 2
+#   end
+# end
