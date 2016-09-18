5f208fe718104d3dd320279e7d3014f4b6c018e2
Renz Mallari <hmmwow2015@gmail.com>
2016-09-18 16:15:10 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+arr_new = []
+   array.each { |num| arr_new << (num ** 2) }
+   return arr_new
+
+# This is my .collect solution that give me 2 failures in 3 Examples
+# arr_new  =  array.collect { |num| num ** 2 }
+# return arr_new
 end
