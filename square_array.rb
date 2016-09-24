9d228a7ff6c71c602000ac59d9eeed51f38f9824
David Weinstein <dwein9@gmail.com>
2016-09-24 01:05:59 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+squares = []
+
+array.each do |x|
+  squares << x**2
 end
+squares
+end
+
+# def square_array(numbers)
+#   numbers.collect { |x| x**2 }
+# end
