5e38c8bcff44d02ed3277fa28fa58d153723c4d2
Zack Wilder <zwilder33@gmail.com>
2016-09-26 00:58:22 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,23 @@
-
-
-def square_array(array)
+numbers = [1,2,3]
+numbers.collect { |n| n ** 2 }
+def square_array(numbers)
     squared = []
-    array.each { |n| squared.push(n ** 2) }
-    return squared
+    numbers.each { |n| squared.push(n ** 2) }
+    squared
   end
 
 square_array([1,2,3])
+
+
+
+
+square_array = [9,10,16,25]
+square_array.collect {|n| n ** 2 }
+
+def square_array(new_numbers)
+  new_squared = []
+  new_numbers.each { |n| new_squared.push(n ** 2) }
+  new_squared
+end
+
+square_array([9,10,16,25])
