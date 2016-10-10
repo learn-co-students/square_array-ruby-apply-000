f84cdcd54b39120fc3afb14151c45411f1784ec3
David Korff <davidkorff@gmail.com>
2016-10-10 15:44:51 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  array.each {|x| x = x*x}
+
+  n= 0
+  while n< array.length
+    array[n] = array[n]*array[n]
+    n +=1
+  end
+  
+  return array
 end
