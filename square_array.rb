582938f9692ecc8e410a25bb1f5af24f2d0b69d8
Daniel Lempesis <danlrcn@hotmail.com>
2016-12-11 22:09:24 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
-  # your code here
+  newarr = []
+  array.each do |i|         #for each item in array:
+    newarr.push(i**2)       #do item^2 and push it into new array
+  end
+return newarr
 end
