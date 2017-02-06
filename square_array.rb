fdeeca4bf352cef3d979709e2168f72e86f3d119
mehdi Jacoby <mah.di@live.com>
2017-02-06 15:59:39 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
-def square_array(n)
-  n.each {|i| i** 2}
+def square_array(array)
+  squared = []
+  array.each {|element| squared << element ** 2}
+  squared
 end
-numbers = [1,2,3]
-square_array(numbers)
-new_numbers = [9,10,16,25]
-square_array(new_numbers)
