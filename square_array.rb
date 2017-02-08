73f2cd995ef6c29672578782d474c1f243fa3a94
Bryan Hou <bryan.hou94@gmail.com>
2017-02-08 20:30:29 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  sq=[]
+  array.each { |k|
+  sq.push(k**2)
+  }
+  return sq
 end
+=begin
+def square_array(array)
+  # your code here
+  array.collect!{ |k| k**2
+  }
+  return array
+end
+=end
