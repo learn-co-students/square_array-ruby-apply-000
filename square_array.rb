62a64d08689e3fe5d1b7d313c6fada0b03f8162b
Chris Dascoli <chris.dascoli@gmail.com>
2017-02-20 15:34:30 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  new_array = [] #create a place to store the values
+  array.each do |int| #iterate over the array
+    new_array.push(int ** 2) #use the push method to store the results in the new_array
+  end
+  return new_array #return the new_array with the squared numbers
+end
+
+def square_array_using_collect(array)
+  array.collect! do |int| #this version of #collect! replaces the int with its new value
+    int ** 2
+  end
+  return array
 end
