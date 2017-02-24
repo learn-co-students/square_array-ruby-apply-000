c9971d642e098cd8930a535bfb5b1066f99fcdfc
Mike Merin <mikemerinweather@gmail.com>
2017-02-24 17:17:20 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+def square_array(a)
+
+  a2 = []
+
+  a.each { |x| a2 << x**2}
+
+  a2
+  
 end
+
+
+def square_array_advanced(a) a.map { |x| x**2 } end
