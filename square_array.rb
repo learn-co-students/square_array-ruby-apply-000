c59b9503020278a686d587515296fe9d8b9f07dd
Benjamin Nathan <bnathan19@gmail.com>
2017-03-01 19:18:25 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  result = []
+  array.each {|el| result << el**2}
+  result
+end
+
+#advanced
+
+def advanced_square_array(array)
+  array.collect {|el| el**2}
 end
