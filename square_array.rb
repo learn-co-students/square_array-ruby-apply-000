e5a054a26f65b91118f0cfa5750d4cc209831978
Evan  Markert <evanmarkert@gmail.com>
2017-03-12 22:52:57 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  arr = []
+  array.each do |index| arr << index ** 2
+  end
+  return arr
 end
+
+
+#def square_array(array)
+#  array.map! do |index| index ** 2
+#end
