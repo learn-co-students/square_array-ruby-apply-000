868f0b541f23c9974c3b8d8f6dddeae46409f599
Hou In Choi <hichoi17@outlook.com>
2017-04-04 17:54:00 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  tempArray=[]
+  array.each do |element|
+    tempArray.push(element*element);
+  end 
+  tempArray
+  
+  #array.collect do |element|
+  #  element*element
+  #end
 end
