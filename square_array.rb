92753433ad5312d1ff0827957d2999eda034fca2
Jermaine Bell <jermainekbell@gmail.com>
2017-04-25 13:51:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
 def square_array(array)
-  # your code here
+  new_numbers = []
+  array.each do |num|
+     new_numbers << num * num
+  end
+  new_numbers
 end
+
+
+
+
+
+
+
+
+
+
+# new_numbers = array.each { |num| num * num }
+# array.each { |num| new_numbers[array.each_index] = num * num }
+# new_numbers.collect { |x| x * x }
