b0feed3baccdc27f97a14075c414e87d2fbf7cc2
bigxshowx <bigxshowx@gmail.com>
2017-05-04 17:22:14 +0900

    .each method tst completes + collect and also .map

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  squared = []
+  array.each{|x| squared.push(x**2)}
+  return squared
 end
+
+def collect(arr)
+	array.collect{|x| x**2}
+end
+
+# I generally prefer to use .map method to change the values of an array
