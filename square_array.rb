c06673afb73c9169430a692cbf90973f851d33fb
Joseph Kim <josephkim0224@gmail.com>
2017-05-15 19:33:26 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  # Higher level
+  # array.map { |element| element ** 2 }
+
+  # using .each
+  new_array = []
+  array.each { |element| new_array.push(element ** 2) }
+  new_array
 end
