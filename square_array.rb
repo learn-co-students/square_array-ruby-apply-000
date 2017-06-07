8beba27d3d29a5a573f79e16c0066e3fa4afb35b
Christine Kong <ckong1792@gmail.com>
2017-06-07 01:35:20 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
+numbers = [1,2,3]
+
 def square_array(array)
-  # your code here
+  a = []
+  array.each do |x| a << x**2
+  end
+  a
 end
+
+square_array(numbers)
+
+new_numbers = [9,10,16,25]
+
+def square_array(array)
+  array.collect do |x| x**2
+  end
+end
+
+square_array(new_numbers)
