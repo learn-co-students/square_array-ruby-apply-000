7e49b9d0c4dd204d6d6deedd00bd91d4e08c4976
William Scripps <wlscripps@gmail.com>
2017-07-10 14:44:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,13 @@
 def square_array(array)
-  array.each {|x| "Dunno man..."}
+  square = []
+  y=0
+  array.each{|x|
+    square[y] = x**2
+    y+=1
+  }
+  return square
+end
+
+def other_square_array(array)
   array.map! {|x| x**2}
 end
