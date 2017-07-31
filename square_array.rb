2200e99604cf775b155af92f253bf2eb3bd86cc8
Abraham Rudy <aberudy@gmail.com>
2017-07-31 18:16:40 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  counter = 0
-  new_array = []
+  counter = 0  #make a counter for our new_array
+  new_array = []   #create our empty new_array
   array.each do |element|
-      new_array[counter] = element**2
-    counter += 1
+      new_array[counter] = element**2 #square each number
+    counter += 1  #next place in our new_array
   end
-return new_array
+return new_array  #return our new_array
 end
