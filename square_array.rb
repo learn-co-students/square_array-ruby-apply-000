34e316fbf82de6d9cd657e1777642a97772d2099
Derek Kwok <dkwok94@gmail.com>
2017-09-08 01:08:20 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
+  #your code here
+  squared_array = []
+  array.each do |number|
+    squared_array.push(number ** 2)
+  end
+  squared_array
 end
+
+#Implemented with the .collect iterator
+#def square_array(array)
+  #array.collect do |number|
+    #number ** 2
+  #end
+#end
+#Tested .collect on IRB and it works (ie. it returns a squared array of the original array).
+#The original array is unchanged as the collect method returns the modified array only.
