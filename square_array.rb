c7b2f17ae80290fbc6c4907600f1d2046449cdef
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-17 18:00:30 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
   squared = []
-  array.each { |num| squared << num*num  }
+  array.each { |num| squared << num*num }
   squared
 end
+
+#def square_array(array)
+#  array.collect { |num| num*num }
+#end
