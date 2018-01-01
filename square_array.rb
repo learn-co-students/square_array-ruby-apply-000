c45b46754a7b70353a2039227494bd34b361d163
Mohammad Khan <sparrowangels@gmail.com>
2018-01-01 07:54:10 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  # a = [ "a", "b", "c" ]
+  # a.each {|x| print x, " -- " }
+
+  # ** Exponent - Performs exponential (power) calculation on operators
+  new_array = []
+  array.each { |x| new_array << x**2}
+  new_array
+
+  # Once you have the tests passing, see if you can implement this with a higher level iterator such as .collect.
+  # a.collect {|x| x + "!" }   #=> ["a!", "b!", "c!", "d!"]
+  # array.collect {|x| x**2}
+
+
 end
