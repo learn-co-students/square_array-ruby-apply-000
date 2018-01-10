5ceb6e5dec5f3133f5e77b677c40856731d0c780
Felipe Leon <felipeleon92@gmail.com>
2018-01-10 20:16:42 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,6 @@
+# I'm defining arr, an empty array to push each element into after it's squared and calling arr to call the proper return value.
 def square_array(array)
-  # your code here
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
