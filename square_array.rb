d836c29c73bd225bf9e770b6b91cc6d807311e25
kai.han@balliol.ox.ac.uk <kai.han@balliol.ox.ac.uk>
2018-04-23 17:09:27 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,10 +3,12 @@ numbers = [1, 2, 3]
 def square_array(array)
   final_array = []
   array.each { |i| final_array << i ** 2 }
+  puts final_array
+end
   
 
 =begin
-this was my higher level iterator, if I didn't comment it out, the tests wouldn't pass
+this was my higher level iterator
 def new_square_array(array)
   new_array = array.collect { |x| x ** 2 }
   puts new_array
