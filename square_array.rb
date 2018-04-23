8db4be2f65c6b364ca5aeb66745ba1a0f9662250
kai.han@balliol.ox.ac.uk <kai.han@balliol.ox.ac.uk>
2018-04-23 17:10:43 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -7,7 +7,13 @@ def square_array(array)
 end
   
 
-
+=begin
+this was my higher level iterator
+def new_square_array(array)
+  new_array = array.collect { |x| x ** 2 }
+  puts new_array
+end
+=end
 
 
 square_array(numbers)
