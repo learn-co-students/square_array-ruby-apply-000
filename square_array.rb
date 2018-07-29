24f890b5e46a30abd5330c3f6b142c305bb37bda
alexanderchao123 <chao.alexander.123@gmail.com>
2018-07-29 22:01:25 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  squared_num_array = []
+  array.each do |num|
+    squared_num = num**2
+    squared_num_array.push(squared_num)
+  end
+  return squared_num_array
 end
