79ddbfbe3f04693606495fabad24a793ef242079
Carlos Sanchez <csan85@outlook.com>
2018-09-18 15:35:34 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+
 def square_array(array)
-  # your code here
+  array1 = []
+  array.each { |i| array1 << i ** 2 }
+  array1
 end
+
+def square_array1(array)
+  array2 = [2, 4, 6]
+  array2.collect { |x| x * x}
+end
+
