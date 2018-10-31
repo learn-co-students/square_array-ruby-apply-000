f74601b481eabffdee57e3ff5afcef0440f613c2
Darnell Simon <darnellatsimon@gmail.com>
2018-10-31 05:04:40 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+# 
+eq = []
 def square_array(array)
-  # your code here
-end
+  array.each do |ele|
+    eq << ele ** 2
+  end
+end 
+
+
+puts square_array([1,2,3])
+puts square_array([9,10,16,25]))
+    # expect(square_array([1,2,3])). => eq([1,4,9])
+    # expect(square_array([9,10,16,25])).=> eq([81,100,256,625])
