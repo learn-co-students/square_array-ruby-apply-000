6802ddb82121f09d2101f0d441592b817037dffc
Shaundra <s.m.crittenden@gmail.com>
2018-11-14 18:24:25 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  arr_squared = []
+  array.each { |x| arr_squared << x ** 2 }
+  arr_squared
 end
+
+# def square_array(array)
+#   array.collect { |x| x ** 2 }
+# end
