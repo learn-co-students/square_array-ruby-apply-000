667f9f001238f5f90a152fec1b7ff4df1cee544b
Matthew Collins <matthew.collins1991@yahoo.com>
2018-11-22 20:06:05 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,9 @@
-array=[]
+
 
 def square_array(array)
- array_new=[]
-  array.each do |x|
-    array_new << x**2
-  end
-  return array_new
+  a=[]
+  array.each do |x| a << x**2
 end
-
+  a
+end
 
