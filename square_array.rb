f7eea7ba09426eacc6f5f5262565d70dc36e381c
Yong Nam Kang <j.yongnamkang@gmail.com>
2018-12-07 01:41:35 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -3,14 +3,9 @@ def square_array(array)
   # answer array.
   new_arr = []
   
-  =begin
   array.each do |array|
     new_arr.push(array * array)
   end
-  =end
-  
-  # above code condensed
-  array.each { |array| new_arr.push(array * array)
   
   return new_arr
 end
