bb5bcc4a1eb0c6d3b9ecc5cd673bfb84b59f89d7
Troy Gooden <tro12185@gmail.com>
2018-12-19 04:27:14 +0000

    solution for square array

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  # This method will take in an array and square them into a new array
+  new_array = []
+  array.each do |square|
+    new_array << square * square
+  end
+    return new_array
 end
