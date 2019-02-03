cf648fcd758c9f5b833cb3410c3944872a5d1c95
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-03 04:51:58 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each do |number|
+    number = number * number
+    squared_array.push(number)
+  end
+  
+  return squared_array
 end
