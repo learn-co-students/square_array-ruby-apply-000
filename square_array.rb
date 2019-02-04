7db4db2f93eae51c6b1f9815c703c5076742bb1c
George Wong <gwong323@yahoo.com>
2019-02-04 21:47:48 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,14 @@
 
-def square_array(array)
-squared = []
-array.each do |number|
-squared << number ** 2
-end
-squared
-end
+def square_array(array) #build square_array method
+squared = []            #create new array named squared
+array.each do |number|  #take each number of array using temporary variable number
+squared << number ** 2  #squares each number and shovel into end array squared
+end #end for each
+squared #outputs array
+end #end for def
 
-#def square_array(array)
+# def square_array(array)
 #  array.collect { |squared| squared ** 2 }
-#end
-
-#square_array([1,2,3])
+# end
+#
+# square_array([1,2,3])
