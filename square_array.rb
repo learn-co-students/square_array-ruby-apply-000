dc7b28f745fbcf320b7731755944811ff4d19207
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-02-26 17:43:54 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |ele| 
+    new_array << ele*ele
+  end
+  return new_array
 end
+
+# def square_array(array)
+#   new_array = []
+#   array.each do |ele|
+#     new_array.push(ele*ele)
+#   end
+#   return new_array
+# end
