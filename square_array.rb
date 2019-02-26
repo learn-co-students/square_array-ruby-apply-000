1787112233ecdc7807d9565c3713e573dfd08660
JENNIFER OH <ohjennifer.90@gmail.com>
2019-02-26 21:59:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  square_array = [ ]
-  array.each do |numbers|
-    square = numbers ** 2
-    square_array << square #.each won't modify the original array, so you have to push the values you create into a new array
+  squared_array = [ ]
+  array.each do |number|
+    squared = number ** 2
+    squared_array << squared #.each won't modify the original array, so you have to push the values you create into a new array
   end
-  square_array #to obtain the return value of a method in the last line read
+  squared_array #to obtain the return value of a method in the last line read
 end
