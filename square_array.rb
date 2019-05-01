948f0a5f15955ea9685edc935f6e5acc0e5c3235
Basiru Danso <basirudns@gmail.com>
2019-05-01 20:43:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,29 +1,29 @@
 def square_arrays(numbers)
-  
   result = []
   
   numbers.each_with_index do|num|
-  square = num * 2
+  square = num ** 2
   
   result << square
   
   end
   return result
-  
 end
 
-puts square_arrays([2, 4, 6, 10])
+print square_arrays([1, 2, 3])
 
 def square_arrays(numbers)
   result = []
-  numbers.map{ |num| num * 2 }
+  numbers.map{ |num| num ** 2 }
 end
 
-puts square_num([10, 20, 30])
+puts square_num([9, 10, 16, 25])
 
 def square_arrays(numbers)
   result =[]
-  numbers.collect { |num| num * num }
+  numbers.collect { |num| num ** num }
 end
 
-puts square_num([3, 9, 12])
+puts square_num([1, 2, 3])
+
+
