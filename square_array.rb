ccb8bb6df66e07f54438683326ba43c989670a0f
DP <pedrozadave@gmail.com>
2019-05-23 17:03:49 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  new_arr = []
+  array.each do |el|
+    new_arr << el**2
+  end
+  new_arr
+end
+
+def square_array_iter(array)
+  new_arr =[] 
+  array.collect {|el| new_arr << el**2}
+  new_arr
 end
