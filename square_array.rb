0fa03981b6e7d915ffa1cb0f7959cd4ad09e4ecc
Lisa LaRochelle <larochelle.lisa@gmail.com>
2019-06-13 03:28:23 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
+original_numbers = [1, 2, 3]
+
 def square_array(array)
-  # your code here
+  
+  new_array = [] #a new array to store the squared numbers
+  
+  array.each do |number|
+    squared_number = number ** 2
+     new_array << squared_number #add/store squared number in the new array
+  end
+  
+  return new_array
+  
 end
+
+square_array(original_numbers)
+
+
