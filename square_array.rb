9067a1f2e67052acac33e276e6aa3f95f56d9e70
Chen Wang <wangcees@gmail.com>
2019-07-26 03:55:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  # arr = []
-  # array.each { |i| arr.push(i*i)}
-  # return arr
+   arr = []
+   array.each { |i| arr.push(i*i)}
+   return arr
   
-  array.collect { |x| x * x}
+  # array.collect { |x| x * x}
 end
