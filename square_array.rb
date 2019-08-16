215fbb13e9710a810b31451ffbe269aa7f0fc4f2
Naveed Ganjani <nganjani@protonmail.com>
2019-08-16 23:34:48 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -6,3 +6,12 @@ def square_array(array)
   end
   return new_array
 end
+
+#Advanced
+def square_with_collect(array)
+  new_array = array.collect { |element| element ** 2 }
+  return new_array
+end
+
+myarr = [ 2, 4, 6 ]
+puts square_with_collect(myarr)
