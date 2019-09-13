012e57986969aae8ce7929551c464a7e1e02c6ca
wise-anchor-7998 <garonhock@yahoo.com>
2019-09-13 17:18:21 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
+  array.each do |ele|
+    answer_array.push(ele * ele)
+  end
+  return answer_array
+  
+  
+
+  
+  
+    
+
 end
+
+puts (square_array([1,2,3]))
+puts(square_array([9,10,16,25]))
