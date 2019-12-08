9b5cf7cc02ac59b7b34e5e6ba1d3854350f45e57
nickie kane <kameo712@gmail.com>
2019-12-08 20:45:13 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  newArray = []
+ newArray = (array.collect{|array| array**2})
+  print newArray
+  puts "\n"
+  
 end
+
+numbers = [1,2,3]
+ 
+square_array(numbers)
+ 
+new_numbers = [9,10,16,25]
+ 
+square_array(new_numbers)
