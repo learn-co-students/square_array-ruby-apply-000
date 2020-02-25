76f54b5a10dac30049ca95119ebfbc3c145cf75a
Marlon Braga <rdg.brg@gmail.com>
2020-02-25 19:21:17 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  newArray = []
+
+  array.each do |num|
+    newArray.push(num ** 2)
+  end
+  return newArray
 end
+
+# def square_array(array)
+#   newArray = array.collect{ |num| (num ** 2)}
+#   return newArray
+# end
