9ca2c562e970550104e8b39fd436574cb1322c2d
John Mura <jjmuradlr@gmail.com>
2020-03-31 01:13:23 +0000

    Done.

---
+++ collect.rb
@@ -0,0 +1,2 @@
+numbers = [1, 2 ,3]
+numbers.collect { |i| i ** 2 }
--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+ sq_array = []
+ array.each { |i| sq_array << i ** 2 }
+ sq_array
 end
+
+numbers = [1, 2, 3]
+square_array(numbers)
+
+new_numbers = [9, 10, 16, 25]
+square_array(new_numbers)
