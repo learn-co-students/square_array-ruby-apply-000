ac4c2910b392dabda343f976b3ffbc3e263979c8
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-15 02:00:56 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+def square_array(list)
+  array = []
+  list.each do |element|
+    square = element ** 2
+    array << square
+  end
+  return array
 end
