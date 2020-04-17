8b9f28de6033a84c2497623f2ddbc9dc1581288e
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-17 01:56:32 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,6 @@
-def square_array(array)
-  # your code here
+numbers = [1,2,3]
+def square_array(numbers)
+  squared_array = []
+  numbers.each {|number| squared_array << number**2 }
+  squared_array
 end
