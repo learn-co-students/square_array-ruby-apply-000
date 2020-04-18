415043c1bdf39ce1d686ff2fc7270dbcfaf1c533
Maysoon Tabali <mtab03@me.com>
2020-04-18 04:23:19 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -3,6 +3,11 @@ def square_array(array)
   new_array = [ ]
   array.each do |num|
     new_array << (num ** 2)
-  end
+ end
   return new_array
 end
+
+#def square_array(numbers)
+#  new_numbers = numbers.collect{|num| num ** 2}
+#  return new_numbers
+#end 
