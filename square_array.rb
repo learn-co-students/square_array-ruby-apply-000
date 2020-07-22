2535b2cb50f385689337d7c4c0f942dde3fb549d
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-07-22 01:34:52 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,29 @@
-def square_array(array)
-  # your code here
+numbers = 
+[
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10  
+]
+
+
+def 
+  
+  square_array(numbers_list)
+  
+                  # your code here
+  
+  squared_numbers = Array.new
+  
+                  #square =
+  
+  numbers_list.each do |number|
+    
+    squared_numbers << (number * number)
+  
+             end
+  
+  
+  squared_numbers
+  
 end
+
+
+square_array(numbers)
