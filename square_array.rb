c8a0fb69316d3b92f4467e1a305499cd93d3c67a
munificent-forum-1695 <bunchshawn@gmail.com>
2020-08-31 17:14:37 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+#require_relative '../square_array'
+
+array = [1,2,3]
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each do |x|
+    x**=2
+    squared_array << x
+  end
+  squared_array
 end
