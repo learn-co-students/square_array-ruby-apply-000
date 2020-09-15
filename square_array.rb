6db3b0ce3f692e2724da2233417c579c14abdc19
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-15 15:46:49 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  array.each do |number|
+    sq_number = number * number
+    array[counter] = sq_number
+    counter += 1
+  end
+end
+
+def square_array2(array)
+  new_array = array.collect{|x| x * x}
 end
